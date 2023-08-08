cbba112bcda4c113184d157cf6678165932e013b
Sebastian Alfieri <sebasalfieri@gmail.com>
2023-08-08 11:31:48 -0300

    wip

---
+++ components/Atropos/Atropos.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import s from './Atropos.module.css';
+
+const Atropos = () => {
+  return <motion.div className={s.container}>index</motion.div>;
+};
+
+export default Atropos;
---
+++ components/index.tsx
@@ -0,0 +1,1 @@
+export { default as Atropos } from './Atropos';
