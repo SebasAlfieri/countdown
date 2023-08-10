b4038a40f8f3e072abc77281c9b5710b7b24d438
Sebastian Alfieri <sebasalfieri@gmail.com>
2023-08-10 13:54:14 -0300

    wip

--- lib/dataset.ts
+++ lib/dataset.ts
@@ -15,13 +15,13 @@ export const dataset: DataSet = {
       name: 'El Gordo',
       day: 27,
       month: 3,
-      img: ''
+      img: '/images/gordo-new.jpg'
     },
     {
       name: 'Laucha',
       day: 13,
       month: 5,
-      img: '/images/back-desktop.jpg'
+      img: '/images/laucha.jpg'
     },
     {
       name: 'Paparella',
@@ -30,34 +30,34 @@ export const dataset: DataSet = {
       img: '/images/paparella.jpg'
     },
     {
-      name: 'Yoru Alfieri',
+      name: 'Yo XD',
       day: 19,
       month: 7,
-      img: '/images/yoru.png'
+      img: '/images/yo.jpg'
     },
     {
       name: 'Maru',
       day: 24,
       month: 7,
-      img: ''
+      img: '/images/maru.jpg'
     },
     {
       name: 'Lucas',
       day: 19,
       month: 8,
-      img: ''
+      img: '/images/lucas.jpg'
     },
     {
       name: 'Veke',
       day: 7,
       month: 9,
-      img: '/images/veke.jpg'
+      img: '/images/veke-new.jpg'
     },
     {
       name: 'Luli',
       day: 17,
       month: 9,
-      img: ''
+      img: '/images/luli.jpg'
     },
     {
       name: 'Axel',
@@ -69,13 +69,13 @@ export const dataset: DataSet = {
       name: 'Leslie',
       day: 17,
       month: 11,
-      img: ''
+      img: '/images/leslie.jpg'
     },
     {
       name: 'Miki',
       day: 24,
       month: 12,
-      img: ''
+      img: '/images/miki.jpg'
     }
   ]
 };
